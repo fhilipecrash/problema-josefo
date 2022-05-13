025f2f31083053001bc928033e884b25d0412111
Fhilipe Coelho <coelhocrash11@gmail.com>
2022-05-13 13:12:54 -0300

    problema de josefo terminado

--- josefo.c
+++ josefo.c
@@ -19,20 +19,27 @@ struct lista {
 void mostrar_soldados(Lista* lista);
 void inserir_soldado(Lista* lista, int total_soldados);
 void remover_soldado(Lista* lista, int num_soldado);
-int josefo(Lista* lista, int pulo);
+void josefo(Lista* lista, int pulo);
 
 void main() {
     // Aloca memória pra lista e pro nó dentro dela e define os valores padrões.
     Lista* lista_soldados = (Lista *)malloc(sizeof(Lista));
     lista_soldados->cabeca = (No *)malloc(sizeof(No));
 
-    lista_soldados->cabeca->anterior = lista_soldados;
-    lista_soldados->cabeca->proximo = lista_soldados;
+    lista_soldados->cabeca->anterior = lista_soldados->cabeca;
+    lista_soldados->cabeca->proximo = lista_soldados->cabeca;
     lista_soldados->cabeca->num = 0;
     lista_soldados->tamanho = 0;
     
-    inserir_soldado(lista_soldados, 10);
+    int resp_soldados;
+    int resp_pulos;
 
+    printf("Insira o número de soldados: ");
+    scanf("%d", &resp_soldados);
+    printf("Insira o tamanho do pulo: ");
+    scanf("%d", &resp_pulos);
+    inserir_soldado(lista_soldados, resp_soldados);
+    josefo(lista_soldados, resp_pulos);
 }
 
 // Usa o tamanho da lista no for para percorrer todos os itens e mostrar os soldados.
@@ -40,8 +47,9 @@ void mostrar_soldados(Lista* lista) {
     No* item = lista->cabeca->proximo;
     int c;
 
+    printf("Soldados: ");
     for (c = 0; c < lista->tamanho; c++) {
-        printf("Soldado: %d\n", item->num);
+        printf("%d ", item->num);
         item = item->proximo;
     }
     printf("\n");
@@ -80,6 +88,7 @@ void inserir_soldado(Lista* lista, int total_soldados) {
             lista->tamanho++;
         }
     }
+    mostrar_soldados(lista);
 }
 
 void remover_soldado(Lista* lista, int num_soldado) {
@@ -93,10 +102,12 @@ void remover_soldado(Lista* lista, int num_soldado) {
             e o próximo soldado recebe o endereço do soldado anterior,
             depois o soldado que não é mais apontado por nada é liberado da memória com free e a execução para.
             */
+            printf("Soldado removido: %d\n", item->num);
             item->anterior->proximo = item->proximo;
             item->proximo->anterior = item->anterior;
             lista->tamanho--;
             free(item);
+            mostrar_soldados(lista);
             break;
         } else {
             item = item->proximo;
@@ -105,11 +116,37 @@ void remover_soldado(Lista* lista, int num_soldado) {
     }
 }
 
-// TODO terminar o problema do josefo
-int josefo(Lista* lista, int pulo) {
+void josefo(Lista* lista, int pulo) {
     No* item = lista->cabeca->proximo;
+    // Nó auxiliar pra guardar o ponteiro do próximo.
+    No* aux;
+    int c = 1;
 
-    while (true) {
-        item = item->proximo;
+    // While 1 (true) faz a lista circular ser lida infinitamente.
+    while (1) {
+        // Ignora o nó de index 0(nó cabeça) e faz apontar pro próximo item da lista.
+        if (item->num != 0) {
+            if (lista->tamanho == 1) {
+                printf("O último soldado foi %d\n", item->num);
+                // Quando restar apenas um elemeno na lista com exceção do nó cabeça o programa para.
+                break;
+            }
+
+            /* 
+            Só remove o soldado quando o valor de C for igual o do pulo, usa a variável auxiliar pra guardar o ponteiro do próximo
+            depois remove e então a variável item recebe o valor guardado na variável auxiliar.
+            */
+            if (c == pulo) {
+                aux = item->proximo;
+                remover_soldado(lista, item->num);
+                c = 1;
+                item = aux;
+            } else {
+                item = item->proximo;
+                c++;
+            }
+        } else {
+            item = item->proximo;
+        }
     }
 }
